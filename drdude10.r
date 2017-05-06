f82cc5e8e56c13d191f3cee473e3a2670f8814b4
DrFredde <fredrock@fredrock.se>
2017-05-06 14:31:12 +0200

    Update drdude10.r

--- drdude10.r
+++ drdude10.r
@@ -1,24 +1,15 @@
-int deg, rng, cnt;
-main()
+int deg, range;
+main ()
 {
-	while(1)
+	while (1)
 	{
-	 	while(rng=scan(deg,15))
-	 	{
-			cannon(deg,rng);
-			drive(deg,100);
-			deg-=10;
-			cnt=0;
-		}
-		cnt++;
-		if (cnt>=2)
-		{
-			if (loc_x()<500) 
-				{if (loc_y()<500) drive(45,100); else drive(315,100);}
-		 	else
-				{if (loc_y()<500) drive(135,100); else drive(225,100);}
-		}
-	  	deg+=10;
-	  	if (deg>=1400) deg-=1400;
+	  while (range=scan(deg,15))
+	  {
+	    drive(deg,100);
+	    cannon(deg,range);
+	    deg-=10;
+	  }
+	  deg+=10;
+	  if (deg>=1400) deg-=1400;
 	}
 }
