af28d92edbfbcb0f862f55dafd9cc49d28814600
kaaswe <jan.larsson@cygate.se>
2017-05-14 14:59:41 +0200

    Add files via upload

--- janne7.r
+++ janne7.r
@@ -1,74 +1,87 @@
-int v_damage, l_speed, from_edge_sv, from_edge_ne, deg, area, count, targ, mcomp, d, candist,quit;
-
-
+int hdev, area, scan_area_count, sa, v_damage, h_speed, l_speed, s_wall, n_wall, deg, targ, mcomp, enemy, candist;
+int s1, s2, s3, s4; /* scan starting points */
 
 main()
 {
-/*v_damage=22;l_speed=30;area=15;count=11;targ=27;mcomp=0;candist=700;*  75% dude*/
-/*v_damage=40;l_speed=80;from_edge_sv=90;from_edge_ne=910;area=15;count=12;targ=28;mcomp=-1;candist=740; 66% dudepro.r*/
-/*area=8;count=10;targ=27;mcomp=0;candist=740;      47 vinster*/
+/* v_damage=22;l_speed=30;area=15;count=11;targ=27;mcomp=0;candist=700;*  75% dude */
+/* v_damage=40;l_speed=80;from_edge_sv=90;from_edge_ne=910;area=15;count=12;targ=28;mcomp=-1;candist=740; 66% dudepro.r */
+/* area=8;count=10;targ=27;mcomp=0;candist=740;      47 vinster */
 
+/*     135    90   45      */
+/*         \  |  /         */
+/*          \ | /          */
+/*    180 --- x --- 0      */
+/*          / | \          */
+/*         /  |  \         */
+/*     225   270   315     */
 
-v_damage=40;
-l_speed=80;
-from_edge_sv=90;
-from_edge_ne=910;
-area=15;
-count=12;
-targ=28;
-mcomp=-1;
+s1=0;
+s2=270;
+s3=180;
+s4=90;
+
+h_speed=100;			/* the speed the bot will have */
+sa=120;					/* Scan area, how wide is the scanner */
+scan_area_count=9;		/* jump the scanner */
+area=10;			    /* scanner size */
+v_damage=40;			/* minimum firering distance, default 40m */
+hdev=15;					/* heading deviation */
+l_speed=70;
+s_wall=350;
+n_wall=650;
+
+targ=25; /* while in target range, back the scanner deg this amount to start over */
+mcomp=3;
 candist=740;
-quit=0;
+deg=90;
 while(1){s();v();n();o();}
 }
 
 s()
 {
-drive(265,100);
-/*deg=180;*/
-  while (loc_y() > from_edge_sv){
-    d=scan(deg,area);
-    if (d > v_damage && d < candist) {cannon(deg-mcomp,d);deg-=targ;}
-    if (deg > 250) deg=100; else deg+=count;
-    if (speed() < l_speed) drive(265,100);
+drive(s2-hdev,h_speed);
+/* deg=s3; */
+  while (loc_y() > s_wall){
+    enemy=scan(deg,area);
+    if (enemy > v_damage && enemy < candist) {cannon(deg-mcomp,enemy);deg-=targ+scan_area_count;}
+    if (deg > s3+sa) deg=s3; else deg+=scan_area_count;
+	drive(s2-hdev,h_speed);
     }
  }
  
 v()
 {
-drive(175,100);
-/*deg=10;*/
- while (loc_x() > from_edge_sv){
-    d=scan(deg,area);
-    if (d > v_damage && d < candist) {cannon(deg-mcomp,d);deg-=targ;}
-    if (deg > 160) deg=10; else deg+=count;
-    if (speed() < l_speed) drive(175,100);
+drive(s3-hdev,h_speed);
+/* deg=s4; */
+ while (loc_x() > s_wall){
+    enemy=scan(deg,area);
+    if (enemy > v_damage && enemy < candist) {cannon(deg-mcomp,enemy);deg-=targ+scan_area_count;}
+    if (deg > s4+sa) deg=s4; else deg+=scan_area_count;
+    drive(s3-hdev,h_speed);
     }
 }
 
 n()
 {
-drive(85,100);
-/*deg=280;*/
- while (loc_y() < from_edge_ne){
-    d=scan(deg,area);
-    if (d > v_damage && d < candist) {cannon(deg-mcomp,d);deg-=targ;}
-    if (deg > 440) deg=280; else deg+=count;
-    if (speed() < l_speed) drive(85,100);
+drive(s4-hdev,h_speed);
+/* deg=s1; */
+ while (loc_y() < n_wall){
+    enemy=scan(deg,area);
+    if (enemy > v_damage && enemy < candist) {cannon(deg-mcomp,enemy);deg-=targ+scan_area_count;}
+    if (deg > s1+sa) deg=s1; else deg+=scan_area_count;
+    drive(s4-hdev,h_speed);
     }
 }
 
 o()
 {
-drive(355,100);
-/*deg=190;*/
- while (loc_x() < from_edge_ne){
-    d=scan(deg,area);
-    if (d > v_damage && d < candist) {cannon(deg-mcomp,d);deg-=targ;}
-    if (deg > 340) deg=190; else deg+=count;
-    if (speed() < l_speed) drive(355,100);
-
+drive(s1-hdev,h_speed);
+/* deg=s2; */
+ while (loc_x() < n_wall){
+    enemy=scan(deg,area);
+    if (enemy > v_damage && enemy < candist) {cannon(deg-mcomp,enemy);deg-=targ+scan_area_count;}
+    if (deg > s2+sa) deg=s2-scan_area; else deg+=scan_area_count;
+    drive(s1-hdev,h_speed);
     }
-
 }
 
